767a7b25434ec963ad3618f5a4be4c3fa22205b5
pablo <pablo.olmos@gmail.com>
2017-05-06 20:42:24 +0100

    Added new collaborator that detects duplicates. After adjusting the all the tests to pass the duplicate detector and mocking it to return what the tests expect, all tests pass; was this cheating? Also, implemented 'correct' logic according to desired behaviour of duplicate detector (but this was not done test first...) -- i'm not sure how i would have come up with is_duplicate() and .duplicates() methods by testing first...

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,25 +5,24 @@ import os
 
 class DuplicateFinder(object):
 
-    def __init__(self, iterator_factory):
+    def __init__(self, iterator_factory, duplicate_detector):
         self._iterator_factory = iterator_factory
+        self._duplicate_detector = duplicate_detector
 
     def find(self, path):
         iterator = self._iterator_factory(path)
         for file in iterator:
-            pass
-        files = os.listdir(path)
-        print(files)
-        if len(files) <= 1:
-            return {}
-        return {'0f79610bebc81f7bfd6212cd656fb467': 2}
+            self._duplicate_detector.is_duplicate(file)
+        return self._duplicate_detector.duplicates()
 
 
 class TestMain(unittest.TestCase):
 
     def setUp(self):
         iterator_factory = mock.Mock(return_value=[])
-        self.duplicate_finder = DuplicateFinder(iterator_factory)
+        duplicate_detector = mock.Mock()
+        duplicate_detector.duplicates = mock.Mock(return_value={'0f79610bebc81f7bfd6212cd656fb467': 2})
+        self.duplicate_finder = DuplicateFinder(iterator_factory, duplicate_detector)
 
     def test_should_find_two_duplicates(self):
         some_path = '/home/pablo/Code/untitled/data'
@@ -38,7 +37,9 @@ class FolderWithOneFileTests(unittest.TestCase):
 
     def setUp(self):
         iterator_factory = mock.Mock(return_value=[])
-        self.duplicate_finder = DuplicateFinder(iterator_factory)
+        duplicate_detector = mock.Mock()
+        duplicate_detector.duplicates = mock.Mock(return_value={})
+        self.duplicate_finder = DuplicateFinder(iterator_factory, duplicate_detector)
 
     def test_should_not_find_duplicates(self):
         some_path = '/home/pablo/Code/untitled/data/a'
@@ -53,7 +54,9 @@ class FolderWithUniqueFilesTests(unittest.TestCase):
 
     def setUp(self):
         iterator_factory = mock.Mock(return_value=[])
-        self.duplicate_finder = DuplicateFinder(iterator_factory)
+        duplicate_detector = mock.Mock()
+        duplicate_detector.duplicates = mock.Mock(return_value={})
+        self.duplicate_finder = DuplicateFinder(iterator_factory, duplicate_detector)
 
     def test_should_not_find_duplicates(self):
         some_path = '/home/pablo/Code/untitled/data/c'
